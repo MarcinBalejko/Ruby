c5e20d324de0fa2e987330adb5f6300692cb5608
MarcinBalejko <45316918+MarcinBalejko@users.noreply.github.com>
2018-12-02 23:52:42 +0800

    Daffy Duckfier
    This program takes user's input and makes it sound like Daffy Duck.

---
+++ daffy.rb
@@ -0,0 +1,23 @@
+puts "Enter your thring pleathe: "
+text = gets.chomp
+while text.empty?
+  puts "No data detected! Pleathe input data: "
+  text = gets.chomp
+end
+
+while
+if text.include? "s" or text.include? "S"
+  text.gsub!(/s/, "th")
+  text.gsub!(/S/, "Th")
+  puts "Your thring: #{text}"
+  puts ""
+  puts "Enter your thring pleathe: "
+  text = gets.chomp
+else
+  puts "Nothing to Daffy Duckify!"
+  text= gets.chomp
+end
+end
+
+
+gets
