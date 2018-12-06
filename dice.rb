02f3a9387344872fc72c1de413cffbd6209fa1b2
MarcinBalejko <45316918+MarcinBalejko@users.noreply.github.com>
2018-12-06 19:13:34 +0800

    Dice
    A program that rolls a dice.

---
+++ dice.rb
@@ -0,0 +1,29 @@
+class Die
+
+  def roll
+    @numberShowing = 1 + rand(6)
+  end
+
+  def showing
+    puts @numberShowing
+  end
+
+end
+
+die = Die.new
+
+command = ""
+
+while command != "exit"
+puts "Do you want to roll the die? (yes/no)"
+command = gets.chomp
+if command == "yes"
+  die.roll
+  die.showing
+elsif command == "no"
+  exit
+end
+end
+
+
+gets
