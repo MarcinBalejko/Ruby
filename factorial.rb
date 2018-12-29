13c641e2a85ee4908c972a6e9b1eef7466f6750d
MarcinBalejko <45316918+MarcinBalejko@users.noreply.github.com>
2018-12-30 01:13:46 +0800

    Factorial
    Factorial method as an integer method.

---
+++ factorial.rb
@@ -0,0 +1,19 @@
+puts 'Enter some number and the factorial of that number will be returned'
+num = gets.chomp.to_i
+
+class Integer
+
+def factorial
+if self < 0
+return 'You can\'t take the factorial of a negative number!'
+end
+
+if self <= 1
+1
+else
+self * (self-1).factorial
+end
+end
+end
+
+puts num.factorial
