566c62a2bd4fa7b7d1d0cb52cabc7e54625082cd
MarcinBalejko <45316918+MarcinBalejko@users.noreply.github.com>
2019-01-06 01:17:38 +0800

    Secret Identities
    A simple code that reveals the secret identities of superheroes.

---
+++ secretidentities.rb
@@ -0,0 +1,12 @@
+
+secret_identities = {
+"The Batman" => "Bruce Wayne",
+"Superman" => "Clark Kent",
+"Wonder Woman" => "Diana Prince",
+"Freakazoid" => "Dexter Douglas"
+}
+
+secret_identities.each do |hero, identity|
+  puts "#{hero}: #{identity}"
+
+end
