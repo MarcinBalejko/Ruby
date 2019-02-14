06d0870d9caf192dc406db0e8377ff8df98aca28
MarcinBalejko <45316918+MarcinBalejko@users.noreply.github.com>
2019-02-14 13:47:14 +0800

    Anagrams
    A program that prints out groups of words that are anagrams.

---
+++ anagrams.rb
@@ -0,0 +1,22 @@
+words =  ['demo', 'none', 'tied', 'evil', 'dome', 'mode', 'live',
+          'fowl', 'veil', 'wolf', 'diet', 'vile', 'edit', 'tide',
+          'flow', 'neon']
+
+def anagrams(array)
+result = {}
+array.each do |word|
+  key = word.split('').sort.join
+  if result.has_key?(key)
+    result[key].push(word)
+  else
+    result[key] = [word]
+  end
+end
+
+result.each_value do |v|
+  puts "------"
+  p v
+end
+end
+
+anagrams(words)
