fdaf867eadcf56866e6361c0d44482af65f516f5
MarcinBalejko <45316918+MarcinBalejko@users.noreply.github.com>
2019-12-05 01:42:41 +0100

    resty
    Calling a JSON API in Ruby

---
+++ resty/apiexample.rb
@@ -0,0 +1,19 @@
+require 'rubygems'
+require 'httparty'
+
+class EdutechionalResty
+    include HTTParty
+    base_uri "edutechional-resty.herokuapp.com/"
+
+    def posts
+        self.class.get('/posts.json')
+    end
+end
+
+edutechional_resty = EdutechionalResty.new
+
+#puts edutechional_resty.posts
+
+edutechional_resty.posts.each do |post|
+    p "Title: #{post['title']} | Description: #{post['description']}"
+end
